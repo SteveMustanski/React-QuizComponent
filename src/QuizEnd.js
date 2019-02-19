a8e0874b62591399400d736700c31645e92b90b5
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-19 16:04:08 -0500

    added quizend and conditional rendering

---
+++ src/QuizEnd.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class QuizEnd extends Component {
+  return() {
+    return (
+      <div>
+      <p>Thanks for playing!</p>
+      <a href=''>Reset Quiz</a>
+    </div>
+    )
+  }
+}
+
+export default QuizEnd;
